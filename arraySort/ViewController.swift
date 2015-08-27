4bfbd31837faef6a931c889b14e8a911794a2935
Marcy <marcyreg@gmail.com>
2015-08-27 01:17:42 -0400

    creating repo of Day 8 class exercise w. Bo -msr

---
+++ arraySort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  arraySort
+//
+//  Created by Marcy Regalado on 8/26/15.
+//  Copyright © 2015 Marcy Regalado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
